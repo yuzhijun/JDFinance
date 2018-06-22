c7197349ba4e41d9efeb4beada1ebc8794f42933
yuzhijun <664281688@qq.com>
2018-06-22 15:55:29 +0800

    header

---
+++ src/pages/home/components/Header.vue
@@ -0,0 +1,54 @@
+<template>
+    <div class="header">
+        <div class="header-left">
+            <span class="iconfont back-icon">&#xe624;</span>
+        </div>
+        <div class="header-input">
+            <span class="iconfont">&#xe632;</span>
+            输入城市/景点/游玩主题
+        </div>
+        <div class="header-right">
+            城市
+            <span class="iconfont arrow-icon">&#xe64a;</span>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeHeader'
+}
+</script>
+
+<style lang="stylus" scoped>
+   @import '~styles/varibles.styl'
+   .header
+      display: flex
+      height: .86rem
+      line-height:.86rem
+      background: $bgColor
+      color: #ffffff
+      .header-left
+        width: .64rem
+        float: left
+        .back-icon
+          text-align: center
+          font-size: .4rem
+      .header-input
+        height: .64rem
+        line-height: .64rem
+        flex: 1
+        background: #ffffff
+        border-radius: .1rem
+        margin-top: .12rem
+        margin-left: .2rem
+        color: #cccccc
+        padding-left: .2rem
+      .header-right
+        width: 1.24rem
+        float: right
+        text-align: center
+        .arrow-icon
+          font-size: .24rem
+          margin-left: -.02rem
+</style>

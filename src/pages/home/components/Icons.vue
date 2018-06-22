1fe6b5972c06ef89c8ca104e7c49e4d41ea9fada
yuzhijun <664281688@qq.com>
2018-06-22 22:10:02 +0800

    icons

---
+++ src/pages/home/components/Icons.vue
@@ -0,0 +1,120 @@
+<template>
+    <div class="icons">
+        <swiper :options="swiperOption">
+            <swiper-slide v-for="(page,index) of pages" :key="index">
+                <div class="icon" v-for="item of page" :key="item.id">
+                    <div class="icon-img">
+                    <img class="icon-imgcontent" :src="item.imgUrl"/>
+                    </div>
+                    <p class="icon-desc">{{item.desc}}</p>
+                </div>
+            </swiper-slide>
+             <!-- Optional controls -->
+            <div class="swiper-pagination"  slot="pagination"></div>
+        </swiper>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeIcons',
+  data () {
+    return {
+      iconList: [{
+        id: '0001',
+        imgUrl: 'http://img1.qunarzz.com/piao/fusion/1803/95/f3dd6c383aeb3b02.png',
+        desc: '景点门票'
+      }, {
+        id: '0002',
+        imgUrl: 'http://img1.qunarzz.com/piao/fusion/1804/5a/13ceb38dcf262f02.png',
+        desc: '一日游'
+      }, {
+        id: '0003',
+        imgUrl: 'http://img1.qunarzz.com/piao/fusion/1804/ff/fdf170ee89594b02.png',
+        desc: '必游榜单'
+      }, {
+        id: '0004',
+        imgUrl: 'http://img1.qunarzz.com/piao/fusion/1803/76/eb88861d78fb9902.png',
+        desc: '动植物园'
+      }, {
+        id: '0005',
+        imgUrl: 'http://img1.qunarzz.com/piao/fusion/1803/6a/45f595250c73d102.png',
+        desc: '夏日玩水'
+      }, {
+        id: '0006',
+        imgUrl: 'http://img1.qunarzz.com/piao/fusion/1805/57/1e29afd06f881102.png',
+        desc: '普陀山'
+      }, {
+        id: '0007',
+        imgUrl: 'http://img1.qunarzz.com/piao/fusion/1803/b1/528a9e80403b8c02.png',
+        desc: '欢乐谷'
+      }, {
+        id: '0008',
+        imgUrl: 'http://img1.qunarzz.com/piao/fusion/1804/ed/cf572be30fc32f02.png',
+        desc: '0+精选'
+      }, {
+        id: '0009',
+        imgUrl: 'http://img1.qunarzz.com/piao/fusion/1804/ed/cf572be30fc32f02.png',
+        desc: '0+精选'
+      }],
+      swiperOption: {
+        pagination: '.swiper-pagination'
+      }
+    }
+  },
+  computed: {
+    pages () {
+      const pages = []
+      this.iconList.forEach((item, index) => {
+        const page = Math.floor(index / 8)
+        if (!pages[page]) {
+          pages[page] = []
+        }
+        pages[page].push(item)
+      })
+      return pages
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+  @import '~styles/varibles.styl'
+  @import '~styles/mixins.styl'
+  .icons >>> .swiper-pagination-bullets
+    bottom: 0rem
+  .icons >>> .swiper-container
+    overflow: inherit
+    width: 100%
+    height: 0
+    padding-bottom: 52%
+  .icon
+    position: relative
+    overflow: hidden
+    height: 0
+    float: left
+    width: 25%
+    padding-bottom: 25%
+    .icon-img
+      position: absolute
+      top: 0
+      left: 0
+      bottom: .44rem
+      right: 0
+      box-sizing: border-box
+      padding: .1rem
+      .icon-imgcontent
+        height: 100%
+        display: block
+        margin: 0 auto
+    .icon-desc
+      width: 100%
+      position: absolute
+      bottom: 0
+      left: 0
+      line-height: .44rem
+      height: .44rem
+      color: $darkTextColor
+      text-align: center
+      ellipsis()
+</style>
--- src/pages/home/components/Swiper.vue
+++ src/pages/home/components/Swiper.vue
@@ -40,7 +40,7 @@ export default {
     width: 100%
     overflow: hidden
     height: 0
-    padding-bottom: 31.25%
+    padding-bottom: 26%
     .swiper-img
         width: 100%
 </style>

0812add49ad2191982a9d036f98c4c34af046692
yuzhijun <664281688@qq.com>
2018-06-23 13:38:33 +0800

    recommend

---
+++ src/pages/home/components/Recommend.vue
@@ -0,0 +1,80 @@
+<template>
+    <div>
+        <div class="title">热销推荐</div>
+        <ul>
+            <li class="item border-bottom" v-for="item of recommendList" :key="item.id">
+                <img class="item-img" :src="item.imgUrl"/>
+                <div class="item-info">
+                  <p class="item-title">{{item.title}}</p>
+                  <p class="item-desc">{{item.desc}}</p>
+                  <button class="item-button">查看详情</button>
+                </div>
+            </li>
+        </ul>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeRecommend',
+  data () {
+    return {
+      recommendList: [{
+        id: '0001',
+        imgUrl: 'http://img1.qunarzz.com/sight/p0/1506/30/7a528fae12c17e6e29d5d13a9d9813ee.water.jpg_200x200_e7f0d0ad.jpg',
+        title: '上海玛雅海滩水公园',
+        desc: '游乐项目惊险刺激，特色表演异彩纷呈'
+      }, {
+        id: '0002',
+        imgUrl: 'http://img1.qunarzz.com/sight/p0/1607/7c/7cda8b6782dabd80b4.img.jpg_200x200_2039e660.jpg',
+        title: '上海迪士尼乐园',
+        desc: '游乐项目惊险刺激，特色表演异彩纷呈'
+      }, {
+        id: '0003',
+        imgUrl: 'http://img1.qunarzz.com/sight/p0/1603/17/170481fb9caf500790.water.jpg_200x200_a32b9a92.jpg',
+        title: '上海野生动物园',
+        desc: '游乐项目惊险刺激，特色表演异彩纷呈'
+      }]
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+  @import '~styles/mixins.styl'
+  .title
+    line-height: .8rem
+    height: .8rem
+    width: 100%
+    text-indent : .2rem
+    background: #eeeeee
+    margin-top: .2rem
+  .item
+    display: flex
+    height: 1.9rem
+    overflow: hidden
+    .item-img
+      width: 1.5rem
+      height: 1.5rem
+      padding: .2rem
+    .item-info
+      flex: 1
+      padding: .1rem
+      min-width: 0
+      .item-title
+        line-height: .54rem
+        font-size: .32rem
+        ellipsis()
+      .item-desc
+        line-height: .4rem
+        font-size: .28rem
+        color: #cccccc
+        ellipsis()
+      .item-button
+        line-height: .44rem
+        margin-top: .22rem
+        background: #ff9300
+        padding: 0 .2rem
+        color: #ffffff
+        border-radius: .07rem
+</style>
